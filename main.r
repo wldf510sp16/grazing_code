ddc98d3ec92ba99bc0795d9b08896d729cfba4a0
timbean <timbean@gmail.com>
2016-03-25 12:41:18 -0700

    Update main.r
    added pseudocode for preliminary data analysis

--- main.r
+++ main.r
@@ -1,3 +1,5 @@
 ### Main R script for conducting analysis / producing figures
 
 print("This script is all about analyzing grazing effects on vertebrates")
+
+## Next step: query the data from the Google Spreadsheet
